5e5b0416d4735a2581557ad1d5c2ecc5d57a54b1
apoichet <apoichet@oui.sncf>
2020-05-03 19:54:48 +0200

    force portarit up orientation

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'starter.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp
+    ]);
     return MaterialApp(
         title: 'FC Parisii',
         showSemanticsDebugger: false,
