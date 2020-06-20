f6839d66a247267ff62b286411a9d92d38d9f0b1
apoichet <apoichet@oui.sncf>
2020-06-20 10:05:24 +0200

    update

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -3,7 +3,6 @@ import 'package:shared_preferences/shared_preferences.dart';
 Future<String> getUser() async {
   SharedPreferences prefs = await SharedPreferences.getInstance();
   return await prefs.get("USER");
-  return await Future.value(null);
 }
 
 setUser(String user) async {
